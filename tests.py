5d676612e159a92d535127e03889783debfd11c2
Michele De Vita <mik3dev@gmail.com>
2017-04-18 01:40:54 +0200

    first commit with wallpaper changer script and some tests

---
+++ tests.py
@@ -0,0 +1,62 @@
+import unittest
+import wallpaper_changer
+
+
+class Mytime:
+    def __init__(self, hour=None, minute=None, second=None):
+        self.second = second
+        self.minute = minute
+        self.hour = hour
+
+
+class Tests(unittest.TestCase):
+    def get_filename(self, path_img) -> str:
+        return path_img.rpartition('/')[2]
+
+    def test_remaining_second_hour_pair_zero_minutes_seconds(self):
+        time = Mytime(4, 0, 0)
+        self.assertEqual(wallpaper_changer.seconds_to_next_img(time), 2 * 60 * 60)
+
+    def test_remaining_seconds_hour_pair_15_minutes_0_seconds(self):
+        time = Mytime(4, 15, 0)
+        self.assertEqual(wallpaper_changer.seconds_to_next_img(time), 60 * 60 + 45 * 60)
+
+    def test_remaining_seconds_hour_pair_30_minutes_20_seconds(self):
+        time = Mytime(4, 30, 20)
+        self.assertEqual(wallpaper_changer.seconds_to_next_img(time), 60 * 60 + 29 * 60 + 40)
+
+    def test_remaining_seconds_hour_pair_50_minutes_50_seconds(self):
+        time = Mytime(8, 50, 50)
+        self.assertEqual(wallpaper_changer.seconds_to_next_img(time), 60 * 60 + 9 * 60 + 10)
+
+    def test_remaining_seconds_hour_dispair_30_minutes_20_seconds(self):
+        time = Mytime(5, 30, 20)
+        self.assertEqual(wallpaper_changer.seconds_to_next_img(time), 29 * 60 + 40)
+
+    def test_remaining_seconds_only_one_second(self):
+        time = Mytime(5, 59, 59)
+        self.assertEqual(wallpaper_changer.seconds_to_next_img(time), 1)
+
+    def test_get_img_path_by_hour1(self):
+        hour = 4
+        path_img = wallpaper_changer.get_img_path_by_hour(hour)
+        filename = self.get_filename(path_img)
+        self.assertTrue(filename.startswith("12"))
+
+    def test_get_img_path_by_hour2(self):
+        hour = 5
+        path_img = wallpaper_changer.get_img_path_by_hour(hour)
+        filename = self.get_filename(path_img)
+        self.assertTrue(filename.startswith("12"))
+
+    def test_get_img_path_by_hour3(self):
+        hour = 6
+        path_img = wallpaper_changer.get_img_path_by_hour(hour)
+        filename = self.get_filename(path_img)
+        self.assertTrue(filename.startswith("01"))
+
+    def test_get_img_path_by_hour4(self):
+        hour = 12
+        path_img = wallpaper_changer.get_img_path_by_hour(hour)
+        filename = self.get_filename(path_img)
+        self.assertTrue(filename.startswith("04"))
---
+++ wallpaper_changer.py
@@ -0,0 +1,81 @@
+import os
+
+import datetime
+
+import time
+
+import sys
+
+from os.path import join as join_paths
+
+import subprocess
+
+def get_resolution():
+    if len(sys.argv) == 1:
+        if os.path.exists(opt_cfg_file):
+            with open(opt_cfg_file) as f:
+                return f.read()
+        else:
+            return subprocess.run("xrandr  | grep \* | cut -d' ' -f4", stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8').replace('\n', '')
+    elif len(sys.argv) == 2:
+        return sys.argv[2]
+    else:
+        message = "Accepted input are:\n- No input: read from %s or get your desktop resolution\n- One argument input:  is your resolution"
+        raise ValueError(message)
+
+
+def change_wallpaper_command(path_img: str) -> str:
+    path_img = path_img if path_img.startswith('file:///') else 'file://' + path_img
+    script = 'string: var Desktops = desktops();' \
+             'for (i=0;i<Desktops.length;i++)' \
+             ' {' \
+             'd = Desktops[i]; ' \
+             'd.wallpaperPlugin = "org.kde.image";' \
+             '  d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");' \
+             ' d.writeConfig("Image", "%s"); ' \
+             '}' % path_img
+    command = "qdbus org.kde.plasmashell /PlasmaShell evaluateScript '%s'" % script
+    return command
+
+
+def is_pair(num) -> bool:
+    return num % 2 == 0
+
+
+def get_img_path_by_hour(hour: int) -> str:
+    hour = hour if is_pair(hour) else hour - 1
+    index_img = pair_hour_to_index[hour]
+    return img_paths[index_img]
+
+
+def seconds_to_next_img(time: {'hour', 'minute', 'second'}) -> int:
+    remaining_hours = 1 if is_pair(time.hour) else 0
+    remaining_minutes = 60 - time.minute
+    remaining_seconds = remaining_minutes * 60 + remaining_hours * 3600 - time.second
+    return remaining_seconds
+
+
+def change_wallpaper():
+    time_now = datetime.datetime.now()
+    img_path = get_img_path_by_hour(time_now.hour)
+    cmd = change_wallpaper_command(img_path)
+    code = os.system(cmd)
+    if code == 512: #widged locked
+        sys.exit(1)
+
+ten_minutes = 600
+
+opt_cfg_file = join_paths(os.environ['HOME'], '.config', 'wallpaper_changer_res.cfg')
+
+resolution = get_resolution()
+path_images = os.environ['HOME'] + '/Pictures/BitDay-2-%s/%s/' % (resolution, resolution)
+img_paths = [join_paths(path_images, img) for img in os.listdir(path_images)]
+
+pair_hour_to_index = dict([(hour, index) for index, hour in enumerate(range(6, 22 + 1, 2))])
+pair_hour_to_index.update({0: 9, 2: 10, 4: 11})
+
+change_wallpaper()
+while True:
+    wait_time = min(ten_minutes, seconds_to_next_img(datetime.datetime.now()))
+    time.sleep(wait_time)
+    change_wallpaper()
